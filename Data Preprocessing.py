734c83eb536f4f1afdc0666adf9c56795988c49c
aperry32 <apdreams30@gmail.com>
2024-08-26 22:07:15 -0400

    removing duplicates and null values

--- Data Preprocessing.py
+++ Data Preprocessing.py
@@ -1,3 +1,21 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
+
+#homes dataset file, locally saved
 home_df = pd.read_csv("C:\\Users\\Arrington Perry\\EDA Project\\Data\\Homes dataset.csv")
+
+#checking info & shape to understand the data better
+home_df.info()
+#home_df.shape
+#home_df.count()
+
+#attemping to drop duplicates 
+home_df.drop_duplicates()
+home_df.count()
+
+#removing empty or null valued rows
+home_df.dropna()
+home_df.info()

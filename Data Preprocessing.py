957fad7beff59d44b260949584273d502c366c7d
aperry32 <apdreams30@gmail.com>
2024-08-26 23:04:17 -0400

    fixed assignment issues

--- Data Preprocessing.py
+++ Data Preprocessing.py
@@ -8,14 +8,22 @@ import numpy as np
 home_df = pd.read_csv("C:\\Users\\Arrington Perry\\EDA Project\\Data\\Homes dataset.csv")
 
 #checking info & shape to understand the data better
-home_df.info()
+#home_df.info()
 #home_df.shape
 #home_df.count()
 
 #attemping to drop duplicates 
-home_df.drop_duplicates()
-home_df.count()
+home_df = home_df.drop_duplicates()
+#home_df.count()
 
 #removing empty or null valued rows
-home_df.dropna()
+home_df = home_df.dropna()
+#home_df.info()
+
+#checking histogram & pairplot for outliers and overall health
+#sns.pairplot(home_df)
+#plt.show()
+
+#dropping feature HOA
+home_df = home_df.drop(['HOA'], axis=1)
 home_df.info()

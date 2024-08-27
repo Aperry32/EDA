e98de4a23853cd1f75b51c4ced672a45d8e89d9d
aperry32 <apdreams30@gmail.com>
2024-08-27 02:59:08 -0400

    converted objects to ints and dropped replaced features from df. Created heatmap and adjusted figure size for readability.

--- Data Preprocessing.py
+++ Data Preprocessing.py
@@ -27,9 +27,22 @@ home_df.info()
 #confirming all null values were removed from df
 home_df.isnull()
 
+#converting object features to floats or integers and then drop the replaced features from df
+home_df['floor_covering_int'] = home_df['floor_covering'].astype('category').cat.codes
+home_df['kitchen_features_int'] = home_df['kitchen_features'].astype('category').cat.codes
+#home_df.info()
+
+home_df = home_df.drop(['kitchen_features', 'floor_covering'], axis=1)
+#home_df.info()
+
 #bar graph of values after null value removal
 home_df.sold_price.value_counts().nlargest(40).plot(kind='bar', figsize=(10,5))
 
+#checking the correlation with a heatmap
+corr = home_df.corr()
+plt.figure(figsize=(10, 8))  # Adjust width and height for readability 
+sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.2f', vmin=-1, vmax=1)
+plt.show()
 
 #checking pairplot for outliers and overall health
 sns.pairplot(home_df)

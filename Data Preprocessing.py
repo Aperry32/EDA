ef673431a3a97b52558d00569ccd9e5ef745c7c6
aperry32 <apdreams30@gmail.com>
2024-08-27 02:31:41 -0400

    added bar graph for sold price

--- Data Preprocessing.py
+++ Data Preprocessing.py
@@ -12,18 +12,28 @@ home_df = pd.read_csv("C:\\Users\\Arrington Perry\\EDA Project\\Data\\Homes data
 #home_df.shape
 #home_df.count()
 
-#attemping to drop duplicates 
-home_df = home_df.drop_duplicates()
-#home_df.count()
-
-#removing empty or null valued rows
-home_df = home_df.dropna()
-#home_df.info()
-
-#checking histogram & pairplot for outliers and overall health
+#checking pairplot for outliers and overall health pre removal of dup & null values
 sns.pairplot(home_df)
 plt.show()
 
-#dropping feature HOA
-home_df = home_df.drop(['HOA'], axis=1)
+#attemping to drop duplicates 
+home_df = home_df.drop_duplicates()
+home_df.count()
+
+#removing empty or null valued rows
+home_df = home_df.dropna()
 home_df.info()
+
+#confirming all null values were removed from df
+home_df.isnull()
+
+#bar graph of values after null value removal
+home_df.sold_price.value_counts().nlargest(40).plot(kind='bar', figsize=(10,5))
+
+
+#checking pairplot for outliers and overall health
+sns.pairplot(home_df)
+plt.show()
+
+
+
